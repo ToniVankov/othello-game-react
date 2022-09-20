c574046256731ca89c5af3171321824f806a4da7
ToniVankov <toni.vankov1@gmail.com>
2022-09-20 23:13:07 +0300

    delete comments

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,46 +4,6 @@ import Square from "./Square";
 import uuid from "react-uuid";
 
 function Grid({ stat, setStat }) {
-  //hook that keep the game turn
-  //const [gameTurn, setGameTurn] = React.useState("white");
-
-  //default or initial states
-  /*
-  const ROWS = rows || 8;
-  const COLS = cols || 8;
-  const INIT = init || [
-    { x: 3, y: 3, state: "white" },
-    { x: 3, y: 4, state: "black" },
-    { x: 4, y: 3, state: "black" },
-    { x: 4, y: 4, state: "white" },
-  ];
-
-  //state array
-  const states = new Array(ROWS);
-  for (let i = 0; i < states.length; i++) {
-    states[i] = new Array(COLS).fill(0);
-  }
-
-  //set default states
-  INIT.map((el) => (states[el.x][el.y] = el.state));
-
-  useEffect(() => {
-    const _states = cellStates.map((x) => [...x]);
-    setCellStates(_states);
-    //*
-    setStat({
-      ...stat,
-      black: 2,
-      white: 2,
-      turn: "white",
-      finished: false,
-    });
-    //* /
-    utils.utilSetAvailableStates(_states, "white");
-  }, []);
-
-  const [cellStates, setCellStates] = useState(states.map((x) => [...x]));
-  */
   const [cellStates, setCellStates] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
